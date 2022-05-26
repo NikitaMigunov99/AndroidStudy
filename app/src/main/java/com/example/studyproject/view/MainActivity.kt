a8dcd5c71ed25055e7c57630236787e8fc8275a2
Nikita Migunov <NAMigunov@sberbank.ru>
2022-05-26 20:57:52 +0300

    Delete all

--- app/src/main/java/com/example/studyproject/view/MainActivity.kt
+++ app/src/main/java/com/example/studyproject/view/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.studyproject.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.studyproject.R
 
@@ -10,10 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.new_main_activity)
-        val intent = Intent(this, FibonacciNumbersActivity::class.java)
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener { startActivity(intent) }
+        setContentView(R.layout.activity_main)
     }
 
 }
